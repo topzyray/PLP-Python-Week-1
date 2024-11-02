a009814f0f9a99c11caaf6ece4d07e1d412c201d
Tope Taiwo <topzyray@yahoo.com>
2024-11-02 15:22:12 +0100

    Solution to assignment

---
+++ user_input.py
@@ -0,0 +1,10 @@
+def main():
+    print("Week 1 assignment on Python Programming at PLP")
+    name = input("What is your name? ")
+    age = input("How old are you? ")
+    location = input("Where is your current location? ")
+
+    print(f"Hello {name}. Your are {age} years of age. How is the weather at {location} today?")
+
+
+main()
