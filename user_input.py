ba540e22b6d74743062beb4da172ee998d65ec37
Tope Taiwo <topzyray@yahoo.com>
2024-11-02 15:36:10 +0100

    Update to assignment

--- user_input.py
+++ user_input.py
@@ -1,10 +1,9 @@
-def main():
-    print("Week 1 assignment on Python Programming at PLP")
-    name = input("What is your name? ")
-    age = input("How old are you? ")
-    location = input("Where is your current location? ")
+print("Week 1 assignment on Python Programming at PLP")
 
-    print(f"Hello {name}. Your are {age} years old. You live at {location}.")
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where is your current location? ")
+
+print(f"Hello {name}. Your are {age} years old. You live at {location}.")
 
 
-main()
